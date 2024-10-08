5bfb67983acb0bf334b2ae72da07a42f2a8177b6
Monyairitshidze <140395495+Monyairitshidze@users.noreply.github.com>
2024-10-08 22:16:10 +0200

    Update LinkedLists.java

--- src/run/LinkedLists.java
+++ src/run/LinkedLists.java
@@ -34,4 +34,34 @@ public class LinkedLists {
         
         System.out.println(node.data);
     }
+
+    public void insertAt(int index, int data){
+
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+
+        Node n = head;
+        for(int i=0; i<index-1; i++){
+          n= n.next;
+        }
+        node.next = n.next;
+        n.next = node;
+    }
+
+
+    public void deleteAt(int index){
+
+        Node n = head;
+        Node n1 = null;
+
+        for(int i=0; i<index-1; i++){
+          n= n.next;
+        }
+
+        n1 = n.next;
+        n.next = n1.next;
+        System.out.println(node.data);
+
+    }
 }
